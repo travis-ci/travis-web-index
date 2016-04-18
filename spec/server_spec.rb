ccd3e4b59b93720c3cf5ec283927f40344e21b50
Buck Doyle <buck@travis-ci.org>
2016-04-18 17:03:51 +0200

    Update descriptions

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "My Sinatra Application" do
-  it "should allow accessing the home page" do
+describe "Redis front-end" do
+  it "should respond" do
     get '/'
     expect(last_response).to be_ok
   end
