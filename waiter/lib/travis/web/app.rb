970d74aa0d0640fc7d575fbb99156440792e28be
Ed Morley <edmorley@users.noreply.github.com>
2017-10-19 15:45:56 +0100

    Use the 'immutable' Cache-Control directive for long-lived assets
    This adds the `immutable` directive to fingerprinted assets that
    already had a 1-year max-age, to avoid unnecessary HTTP 304 requests.
    
    Before:
    `Cache-Control: public, max-age=31536000`
    
    After:
    `Cache-Control: public, max-age=31536000, immutable`
    
    See:
    https://hacks.mozilla.org/2017/01/using-immutable-caching-to-speed-up-the-web/
    https://code.facebook.com/posts/557147474482256/this-browser-tweak-saved-60-of-requests-to-facebook/
    
    Is the travis-web-index port of travis-ci/travis-web#1375,
    which refs travis-ci/travis-ci#8601.

--- waiter/lib/travis/web/app.rb
+++ waiter/lib/travis/web/app.rb
@@ -162,7 +162,7 @@ class Travis::Web::App
     def cache_control(file)
       case path_for(file)
       when '/'        then "public, must-revalidate, max-age=0"
-      else "public, max-age=#{age}"
+      else "public, max-age=#{age}, immutable"
       end
     end
 
